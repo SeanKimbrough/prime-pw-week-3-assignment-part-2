67137199891a5ba20038f85bf61e413461fbb92b
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2022-12-18 16:12:05 -0600

    finished questions up until 2a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -15,25 +15,25 @@ for (let i=0; i<4; i++) {
 console.log('count from 0 to 5');
 for (let i = 0; i<6; i++){
   console.log(i);
-} // have to change the i is true as long as it is lower than 6.
-
+} // have to change the i is true and will run 5 times while i is less thsn 6
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
 for (let i=3; i<6; i++){
   console.log(i);
-} // have to change the i to 3, and the i is going try as long as it is lower and 6 and greater than 3.
-
+} // have to change the i to 3, and the i is going run from 3 times from 3 to 5.
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 for (let even = 2; even <= 10; even ++){
   console.log(even++);
-} // have to change the i variable and change it to the i is true as long as the even numbers are less than or equal to 10.
-
+} // have to change the i variable and change it to the i is going to run until it runs all even numbers from 2 to 10.
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
+for (let i=5; i>0; i--){
+  console.log(i--);
 
+} // changed i variable to 5 and rule to i for 5 times until 0.
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -41,7 +41,10 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for (let i=0; i<stars.length; i++){
+  console.log('Some Stars:' , stars);
+  stars++
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
